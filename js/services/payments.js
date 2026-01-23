c3aaad3a782ddf7a5d8929a564e62dbebd122f89
alexwboles <alexwboles@gmail.com>
2026-01-22 19:58:41 -0500

    Delete js/services directory

--- js/services/payments.js
+++
@@ -1,9 +0,0 @@
-// js/services/payments.js
-export async function createRooferPayout(rooferId, amountCents) {
-  const res = await fetch('/functions/stripe-create-payout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ rooferId, amountCents })
-  });
-  return await res.json();
-}
