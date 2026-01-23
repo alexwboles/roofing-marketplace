320b8c946b58fca0b171fe85efd2d5f47f1bcaa8
alexwboles <alexwboles@gmail.com>
2026-01-22 20:13:46 -0500

    Update router.js wrangler fix

--- js/router.js
+++ js/router.js
@@ -1,1 +1,68 @@
+import { renderHomeView } from "./views/home.js";
+import { renderIntakeView } from "./views/intake.js";
+import { renderHomeownerDashboardView } from "./views/homeownerDashboard.js";
+import { renderRooferDashboardView } from "./views/rooferDashboard.js";
+
+let appRoot = null;
+
+const routes = {
+  "/": "home",
+  "/home": "home",
+  "/intake": "intake",
+  "/homeowner-dashboard": "homeownerDashboard",
+  "/roofer-dashboard": "rooferDashboard",
+};
+
+export function initRouter(root, routeMap) {
+  appRoot = root;
+
+  window.addEventListener("popstate", () => {
+    handleRoute(location.pathname);
+  });
+
+  handleRoute(location.pathname);
+}
+
+export function navigateTo(routeKey) {
+  let path = "/";
+  switch (routeKey) {
+    case "home":
+      path = "/";
+      break;
+    case "intake":
+      path = "/intake";
+      break;
+    case "homeownerDashboard":
+      path = "/homeowner-dashboard";
+      break;
+    case "rooferDashboard":
+      path = "/roofer-dashboard";
+      break;
+  }
+  history.pushState({}, "", path);
+  handleRoute(path);
+}
+
+function handleRoute(pathname) {
+  if (!appRoot) return;
+
+  const routeKey = routes[pathname] || "home";
+
+  switch (routeKey) {
+    case "home":
+      renderHomeView(appRoot);
+      break;
+    case "intake":
+      renderIntakeView(appRoot);
+      break;
+    case "homeownerDashboard":
+      renderHomeownerDashboardView(appRoot);
+      break;
+    case "rooferDashboard":
+      renderRooferDashboardView(appRoot);
+      break;
+    default:
+      renderHomeView(appRoot);
+  }
+}
 
