f0a31274775679377f9d6fe7018a3eba2d51f316
alexwboles <alexwboles@gmail.com>
2026-02-04 13:28:56 -0500

    Create fixLeaflet.js

---
+++ frontend/lib/fixLeaflet.js
@@ -0,0 +1,10 @@
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "/leaflet/images/marker-icon-2x.png",
+  iconUrl: "/leaflet/images/marker-icon.png",
+  shadowUrl: "/leaflet/images/marker-shadow.png",
+});
