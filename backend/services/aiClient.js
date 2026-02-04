a444b1f7af0bb4e7c72867bdc1cad0d4722629c0
alexwboles <alexwboles@gmail.com>
2026-02-04 12:48:39 -0500

    Update aiClient.js

--- backend/services/aiClient.js
+++ backend/services/aiClient.js
@@ -1,1 +1,9 @@
+import axios from "axios";
 
+export async function analyzeRoof(address) {
+  const response = await axios.post(
+    process.env.AI_SERVICE_URL + "/analyze",
+    { address }
+  );
+  return response.data;
+}
