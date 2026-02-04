55d9910132c7a6f85e4bb38b3ba9bb0644ae108e
alexwboles <alexwboles@gmail.com>
2026-02-04 13:01:41 -0500

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,1 +1,9 @@
+import { Router } from "express";
+import { login, registerContractor } from "../controllers/authController.js";
 
+const router = Router();
+
+router.post("/login", login);
+router.post("/register-contractor", registerContractor);
+
+export default router;
