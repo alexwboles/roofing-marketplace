0ddadd7634ee61ce103ddf7e7e5c7c6865dcc356
alexwboles <alexwboles@gmail.com>
2026-02-04 12:58:29 -0500

    Update and rename RoofingAnalysis.js to RoofAnalysis.js

---
+++ backend/models/RoofAnalysis.js
@@ -0,0 +1,21 @@
+import db from "../utils/db.js";
+
+export async function upsertRoofAnalysis(leadId, data) {
+  const { rows } = await db.query(
+    `INSERT INTO roof_analysis (lead_id, data)
+     VALUES ($1, $2)
+     ON CONFLICT (lead_id)
+     DO UPDATE SET data = EXCLUDED.data
+     RETURNING *`,
+    [leadId, data]
+  );
+  return rows[0];
+}
+
+export async function getRoofAnalysisByLeadId(leadId) {
+  const { rows } = await db.query(
+    "SELECT * FROM roof_analysis WHERE lead_id = $1 LIMIT 1",
+    [leadId]
+  );
+  return rows[0] || null;
+}
