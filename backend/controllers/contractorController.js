85365e83105fc2a2d68442dd83d8a04d15495196
alexwboles <alexwboles@gmail.com>
2026-02-04 13:00:46 -0500

    Update contractorController.js

--- backend/controllers/contractorController.js
+++ backend/controllers/contractorController.js
@@ -1,1 +1,16 @@
+import { upsertContractorPricing } from "../models/ContractorPricing.js";
+import { requireFields } from "../utils/validators.js";
 
+export async function updatePricing(req, res, next) {
+  try {
+    const contractorId = req.user.id; // assuming user.id == contractor_id or mapped later
+    requireFields(req.body, ["base_price_per_square"]);
+
+    const pricing = req.body;
+    const record = await upsertContractorPricing(contractorId, pricing);
+
+    res.json({ pricing: record.data });
+  } catch (err) {
+    next(err);
+  }
+}
