4a882dc9b51c543c00e794432c32548bf9e07e1b
alexwboles <alexwboles@gmail.com>
2026-02-04 12:58:50 -0500

    Update Quote.js

--- backend/models/Quote.js
+++ backend/models/Quote.js
@@ -1,1 +1,24 @@
+import db from "../utils/db.js";
 
+export async function createQuote({
+  leadId,
+  contractorId,
+  amount,
+  metadata
+}) {
+  const { rows } = await db.query(
+    `INSERT INTO quotes (lead_id, contractor_id, amount, metadata)
+     VALUES ($1, $2, $3, $4)
+     RETURNING *`,
+    [leadId, contractorId, amount, metadata]
+  );
+  return rows[0];
+}
+
+export async function getQuotesByLeadId(leadId) {
+  const { rows } = await db.query(
+    "SELECT * FROM quotes WHERE lead_id = $1",
+    [leadId]
+  );
+  return rows;
+}
