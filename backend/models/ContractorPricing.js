b3ead3d10dcd969e66e959eb4c478f8ca2fe7e4c
alexwboles <alexwboles@gmail.com>
2026-02-04 12:57:32 -0500

    Update ContractorPricing.js

--- backend/models/ContractorPricing.js
+++ backend/models/ContractorPricing.js
@@ -1,1 +1,18 @@
+import db from "../utils/db.js";
 
+export async function upsertContractorPricing(contractorId, pricingData) {
+  const { rows } = await db.query(
+    `INSERT INTO contractor_pricing (contractor_id, data)
+     VALUES ($1, $2)
+     ON CONFLICT (contractor_id)
+     DO UPDATE SET data = EXCLUDED.data
+     RETURNING *`,
+    [contractorId, pricingData]
+  );
+  return rows[0];
+}
+
+export async function getAllContractorPricing() {
+  const { rows } = await db.query("SELECT * FROM contractor_pricing");
+  return rows;
+}
