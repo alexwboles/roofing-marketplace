0ce5f453824893e8a449a03b09eafe12e7a25853
alexwboles <alexwboles@gmail.com>
2026-02-04 12:54:44 -0500

    Update errorHandler.js

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,1 +1,9 @@
+import { logError } from "../utils/logger.js";
 
+export default function errorHandler(err, req, res, next) {
+  logError("Unhandled error", { err });
+  const status = err.status || 500;
+  res.status(status).json({
+    error: err.message || "Internal server error"
+  });
+}
