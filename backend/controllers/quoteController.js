eb6f3add204487377bf70833843c07c36dc644d0
alexwboles <alexwboles@gmail.com>
2026-02-04 12:49:47 -0500

    Update quoteController.js

--- backend/controllers/quoteController.js
+++ backend/controllers/quoteController.js
@@ -1,1 +1,22 @@
+import db from "../utils/db.js";
+import { generateQuote } from "../services/pricingEngine.js";
 
+export async function generateQuotes(req, res) {
+  const { leadId } = req.body;
+
+  const roof = await db.query(
+    "SELECT data FROM roof_analysis WHERE lead_id = $1",
+    [leadId]
+  );
+
+  const contractors = await db.query(
+    "SELECT * FROM contractor_pricing"
+  );
+
+  const quotes = contractors.rows.map(c => ({
+    contractorId: c.contractor_id,
+    bid: generateQuote(roof.rows[0].data, c.data),
+  }));
+
+  res.json({ quotes });
+}
