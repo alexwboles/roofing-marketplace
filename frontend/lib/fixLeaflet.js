fb2e0549f8fb0261d7326ce78e6bb4fd2fd40cc0
alexwboles <alexwboles@gmail.com>
2026-02-06 10:59:13 -0500

    Update fixLeaflet.js

--- frontend/lib/fixLeaflet.js
+++ frontend/lib/fixLeaflet.js
@@ -6,5 +6,5 @@ delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: "/leaflet/images/marker-icon-2x.png",
   iconUrl: "/leaflet/images/marker-icon.png",
-  shadowUrl: "/leaflet/images/marker-shadow.png",
+  shadowUrl: "/leaflet/images/marker-shadow.png"
 });
